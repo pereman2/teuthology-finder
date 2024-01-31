bf361b1d9a95f08a6fd1b22570e2d69f13a48663
Pere Diaz Bou <pere-altea@hotmail.com>
2024-01-31 19:17:58 +0100

    slim shady

--- search.py
+++ search.py
@@ -2,6 +2,7 @@
 
 
 import requests
+import datetime
 import sys
 url = sys.argv[1]
 
@@ -29,6 +30,22 @@ print('Found {len(log_links)} failing runs')
 print('------------------------------------------')
 
 keywords = ['Segmentation fault', 'ceph_assert', 'Caught signal (Aborted)', 'Traceback']
+errors = {}
+def without_prefix(s):
+    split = s.split(' ')
+    is_log_line = False
+    try:
+        datetime.datetime.fromisoformat(split[0])
+        is_log_line = True
+    except:
+        pass
+    if is_log_line:
+        without_date = ' '.join(split[1:])
+        split_machine = without_date.split(':')
+        without_prefix = ' '.join(split_machine[2:])
+        return without_prefix
+    return s
+
 for link in log_links:
     print('------------------------------------------')
     print(f'Getting info from {link}')
@@ -40,12 +57,25 @@ for link in log_links:
         line = lines[i]
         for keyword in keywords:
             if keyword in line:
-                # print next 20 lines
-                for j in range(i, i+20):
+                # print next 30 lines
+                error = ""
+                for j in range(i, i+30):
                     if j < len(lines):
                         print(lines[j])
+                        error += without_prefix(lines[j]) + "\n" 
                     else:
                         break
+                if error not in errors:
+                    errors[error] = 1
+                else:
+                    errors[error] += 1
                 break
         i += 1
 
+import json
+print(json.dumps(errors, indent=2))
+for k, v in errors.items():
+    print()
+    print()
+    print("Error count = ", v)
+    print(k)
